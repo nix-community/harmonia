2b3ba10b9461c5899952b3ae7bde97f2ee8cbdb0
John Ericson <John.Ericson@Obsidian.Systems>
2025-11-10 22:27:39 -0500

    Add more `DerivedPath` uspstream JSON tests

--- harmonia-store-core/tests/json_upstream.rs
+++ harmonia-store-core/tests/json_upstream.rs
@@ -104,6 +104,51 @@ fn test_derived_path_opaque() {
 }
 
 #[test]
+fn test_derived_path_built() {
+    test_upstream_json(
+        "derived-path/mutli_built.json",
+        DerivedPath::Built {
+            drv_path: SingleDerivedPath::Opaque(
+                "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+            ),
+            outputs: OutputSpec::Named(["bar", "baz"].into_iter().map(|s| s.parse().unwrap()).collect()),
+        },
+    );
+}
+
+#[test]
+fn test_derived_path_built_built() {
+    test_upstream_json(
+        "derived-path/multi_built_built.json",
+        DerivedPath::Built {
+            drv_path: SingleDerivedPath::Built {
+                drv_path: Box::new(SingleDerivedPath::Opaque(
+                    "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+                )),
+                output: "bar".parse().unwrap(),
+            },
+            outputs: OutputSpec::Named(["baz", "quux"].into_iter().map(|s| s.parse().unwrap()).collect()),
+        },
+    );
+}
+
+#[test]
+fn test_derived_path_built_built_wildcard() {
+    test_upstream_json(
+        "derived-path/multi_built_built_wildcard.json",
+        DerivedPath::Built {
+            drv_path: SingleDerivedPath::Built {
+                drv_path: Box::new(SingleDerivedPath::Opaque(
+                    "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+                )),
+                output: "bar".parse().unwrap(),
+            },
+            outputs: OutputSpec::All,
+        },
+    );
+}
+
+#[test]
 fn test_realisation_simple() {
     use harmonia_store_core::realisation::DrvOutput;
 
