4af498897dba700fd4a7135b0374706b1b3ae213
John Ericson <John.Ericson@Obsidian.Systems>
2025-12-05 18:30:33 -0500

    Newer Nix
    `base16` is now the default hash format. Make sure it is called that and
    not `hex`, which upstream Nix doesn't know about.

--- harmonia-store-core/tests/json_upstream.rs
+++ harmonia-store-core/tests/json_upstream.rs
@@ -223,21 +223,21 @@ test_upstream_json!(
     }
 );
 
-// Base64 is the canonical/normal form, so test both read and write
+// Base16/hex is the canonical form, so test both read and write
 test_upstream_json!(
-    test_hash_sha256_base64,
-    libutil_test_data_path("hash/sha256-base64.json"),
+    test_hash_sha256_base16,
+    libutil_test_data_path("hash/sha256-base16.json"),
     Hash::new(
         Algorithm::SHA256,
         &hex!("f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b"),
     )
 );
 
-// Base16/hex is not the canonical form - read-only test
+// Base64 is not the canonical form - read-only test
 #[test]
-fn test_hash_sha256_base16_from_json() {
+fn test_hash_sha256_base64_from_json() {
     test_upstream_json_from_json(
-        &libutil_test_data_path("hash/sha256-base16.json"),
+        &libutil_test_data_path("hash/sha256-base64.json"),
         &Hash::new(
             Algorithm::SHA256,
             &hex!("f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b"),
