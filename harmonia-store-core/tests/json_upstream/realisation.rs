0fb6d9a9e1184e07f3a3b2bdd67585c29adf0f52
Amaan Qureshi <git@amaanq.com>
2026-03-03 13:06:49 -0500

    realisation: restructure to match upstream key-value format

--- harmonia-store-core/tests/json_upstream/realisation.rs
+++ harmonia-store-core/tests/json_upstream/realisation.rs
@@ -2,41 +2,69 @@
 
 use crate::libstore_test_data_path;
 use crate::test_upstream_json;
-use harmonia_store_core::realisation::{DrvOutput, Realisation};
+use harmonia_store_core::realisation::{DrvOutput, Realisation, UnkeyedRealisation};
+use harmonia_store_core::signature::Signature;
+
+fn test_sig() -> Signature {
+    Signature::from_parts("asdf", &[0u8; 64]).unwrap()
+}
+
+// ========== Realisation ==========
 
 test_upstream_json!(
     test_realisation_simple,
     libstore_test_data_path("realisation/simple.json"),
     {
         Realisation {
-            id: "sha256:ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad!foo"
-                .parse::<DrvOutput>()
-                .unwrap(),
-            out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
-            signatures: Default::default(),
-            dependent_realisations: Default::default(),
+            key: DrvOutput {
+                drv_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-bar.drv".parse().unwrap(),
+                output_name: "foo".parse().unwrap(),
+            },
+            value: UnkeyedRealisation {
+                out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo".parse().unwrap(),
+                signatures: Default::default(),
+            },
         }
     }
 );
 
 test_upstream_json!(
-    test_realisation_with_dependent,
-    libstore_test_data_path("realisation/with-dependent-realisations.json"),
+    test_realisation_with_signature,
+    libstore_test_data_path("realisation/with-signature.json"),
     {
         Realisation {
-            id: "sha256:ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad!foo"
-                .parse::<DrvOutput>()
-                .unwrap(),
-            out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+            key: DrvOutput {
+                drv_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-bar.drv".parse().unwrap(),
+                output_name: "foo".parse().unwrap(),
+            },
+            value: UnkeyedRealisation {
+                out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+                signatures: [test_sig()].into(),
+            },
+        }
+    }
+);
+
+// ========== UnkeyedRealisation ==========
+
+test_upstream_json!(
+    test_unkeyed_realisation_simple,
+    libstore_test_data_path("realisation/unkeyed-simple.json"),
+    {
+        UnkeyedRealisation {
+            out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo".parse().unwrap(),
             signatures: Default::default(),
-            dependent_realisations: [(
-                "sha256:ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad!foo"
-                    .parse::<DrvOutput>()
-                    .unwrap(),
-                "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
-            )]
-            .into_iter()
-            .collect(),
+        }
+    }
+);
+
+test_upstream_json!(
+    test_unkeyed_realisation_with_signature,
+    libstore_test_data_path("realisation/unkeyed-with-signature.json"),
+    {
+        UnkeyedRealisation {
+            out_path: "g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-foo.drv".parse().unwrap(),
+            signatures: [test_sig()].into(),
         }
     }
 );
